c54854148685c9ff06db3a537d69619899821485
Rodinei <rodinei.lima@escala-app.com>
2025-06-17 19:47:23 -0300

    adiciona deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/saudediaria/", // substitua pelo nome real do seu repo no GitHub
   plugins: [react()],
-})
+});
